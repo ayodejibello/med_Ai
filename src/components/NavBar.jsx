c4440943c408fd661ff92d2d8c362e21a9a23973
Abdullah <abdullahayodeji005@gmail.com>
2025-07-01 23:41:25 +0100

    Added homepage mobile styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,53 @@
 import { Link } from "react-router-dom";
+import menuIcon from "../assets/hamburger-icon.svg";
+import close from "../assets/close_hover.png";
+import { useState } from "react";
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="flex justify-between p-10 items-center">
       <div className="text-3xl font-bold">Mednova Ai</div>
-      <div>
+      <div className="md:block hidden">
         <ul className="flex items-center gap-4">
           <li>Features</li>
           <li>About Us</li>
           <li>Contact Us</li>
         </ul>
       </div>
-      <div>
-        <button className="border-5 rounded-sm border-gray-600 p-4">
+      <div className="">
+        <button className="border-5 rounded-sm border-gray-600 p-4 md:block hidden">
           <Link to={"/login"}>Login/Signup</Link>
         </button>
+        <button
+          className="border-2 rounded-sm border-gray-200 p-4 md:hidden block"
+          onClick={() => {
+            setShowMenu(true);
+          }}
+        >
+          <img src={menuIcon} alt="menu" />
+        </button>
       </div>
+      {/* mobile menu */}
+      {showMenu && (
+        <div className="w-2/3 flex flex-col h-screen fixed z-10 top-0 right-0 bg-gray-900 p-2">
+          <button
+            className="border-2 rounded-sm border-gray-200 p-2 self-start w-max"
+            onClick={() => {
+              setShowMenu(false);
+            }}
+          >
+            <img src={close} alt="menu" />
+          </button>
+          <ul className="flex flex-col items-center gap-4 mt-10">
+            <li>Features</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <button className="border-2 rounded-sm border-gray-500 p-2 text-sm">
+              <Link to={"/login"}>Login/Signup</Link>
+            </button>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ import { Link } from "react-router-dom";
 const ProfileCard = ({ image, name, position }) => {
   return (
     <div className="flex flex-col items-center gap-4 font-semibold text-xl">
-      <img src={image} alt={name} className="rounded-full size-56" />
+      <img src={image} alt={name} className="rounded-full size-52" />
       <span>{name}</span>
       <span>{position}</span>
     </div>
@@ -25,41 +25,41 @@ const Home = () => {
   return (
     <div className="homepage min-h-screen text-white">
       <NavBar />
-      <div className="mt-10 p-10">
+      <div className="mt-10">
         <h1 className="text-6xl font-bold text-center">MedNova AI</h1>
         <h2 className="text-lg font-light text-center w-2/3 mx-auto mt-4">
           Empowers clinicans, optimize operations, and deliver exceptional care
           with cutting edge AI technology
         </h2>
         {/*  */}
-        <div className="my-20 py-20">
+        <div className="my-20 py-20 px-10">
           <div
-            className="h-96 min-w-[380px] border-[1px] border-gray-700 rounded-sm flex self-center justify-self-center relative bg-center bg-contain bg-no-repeat"
+            className="h-96 md:w-1/3 w-full md:border-[1px] border-none border-gray-700 rounded-sm flex self-center justify-self-center md:relative bg-center bg-contain bg-no-repeat"
             style={{ backgroundImage: `url(${r2})` }}
           >
-            <div className="w-max absolute -left-[70%] -top-10 flex justify-center  flex-col rounded-sm ">
+            <div className="w-max absolute -left-[70%] -top-10 md:flex hidden  justify-center  flex-col rounded-sm ">
               <h1 className="text-4xl font-bold">Mednova AI,</h1>
               <h2 className="text-3xl font-bold text-lime-400">
                 Accelerates diagnostics
               </h2>
             </div>
-            <div className="w-60 h-20 bg-gray-700 text-white absolute -right-[60%] -top-10 flex justify-center items-center flex-col rounded-sm">
+            <div className="w-60 h-20 bg-gray-700 text-white absolute -right-[50%] -top-10 md:flex hidden justify-center items-center flex-col rounded-sm">
               <h1 className="text-gray-300">Health score</h1>
               <h2 className="text-2xl font-semibold tracking-wide">8/10</h2>
             </div>
-            <div className="w-60 h-20 bg-gray-700 text-white absolute -left-[60%] bottom-10  flex justify-center items-center flex-col rounded-sm">
+            <div className="w-60 h-20 bg-gray-700 text-white absolute -left-[50%] bottom-10  md:flex hidden justify-center items-center flex-col rounded-sm">
               <h1 className="text-gray-300">Heart rate</h1>
               <h2 className="text-2xl font-semibold tracking-wide">70BPM</h2>
             </div>
-            <div className="w-60 h-20 bg-gray-700 text-white absolute -right-[60%] -bottom-10  flex justify-center items-center flex-col rounded-sm">
+            <div className="w-60 h-20 bg-gray-700 text-white absolute -right-[50%] -bottom-10  md:flex hidden justify-center items-center flex-col rounded-sm">
               <h1 className="text-gray-300">Heart rate</h1>
               <h2 className="text-2xl font-semibold tracking-wide">70BPM</h2>
             </div>
           </div>
         </div>
 
-        {/*  */}
-        <div className="grid grid-cols-2 gap-6 w-4/5 mx-auto mt-20 ">
+        {/* the 4 box rating section  */}
+        {/* <div className="grid grid-cols-2 gap-6 w-4/5 mx-auto mt-20 ">
           <div className="border-1 h-60 flex flex-col justify-center items-center rounded-xl border-gray-500 scaler">
             <span className="text-gray-200">Total users</span>
             <span className="text-4xl font-bold text-amber-300">+258k</span>
@@ -76,7 +76,7 @@ const Home = () => {
             <span className="text-gray-200">ratings</span>
             <span className="text-4xl font-bold text-amber-300">4.8</span>
           </div>
-        </div>
+        </div> */}
         {/*  */}
         <h1 className="text-center text-3xl mt-16 mb-4">
           Ask mednova anything
@@ -91,61 +91,62 @@ const Home = () => {
         </Link>
 
         {/*  */}
-        <h1 className="text-4xl font-bold text-center mt-32 mb-8">
-          Revolutionalize healthcare with AI
-        </h1>
-        <p className="text-lg font-light text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quae
-          ipsa alias amet impedit vel at autem voluptas? Corrupti facilis
-          aspernatur tenetur fugit voluptatibus nemo ducimus laborum similique
-          numquam doloremque!
-        </p>
-        {/*  */}
+        <div className="p-10 px-20">
+          <h1 className="text-4xl font-bold text-center mt-32 mb-8">
+            Revolutionalize healthcare with AI
+          </h1>
+          <p className="text-lg font-light text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae ipsa alias amet impedit vel at autem voluptas? Corrupti facilis
+            aspernatur tenetur fugit voluptatibus nemo ducimus laborum similique
+            numquam doloremque!
+          </p>
+          {/*  */}
 
-        <div className="flex items-center gap-10 justify-between w-full pb-32 mt-20">
-          <div className="flex flex-col items-center justify-center gap-12">
-            <img src={heart} className="w-44 scaler" />
-            <div className="border-1 rounded-md p-5 text-xl ">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-              obcaecati impedit est assumenda repellat nihil ea fugit magni?
-              Commodi harum illum magni provident, quibusdam laboriosam
-              voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Nisi, doloribus.
+          <div className="flex md:flex-row flex-col items-center gap-10 justify-between w-full pb-32 mt-20">
+            <div className="flex flex-col items-center justify-center gap-12">
+              <img src={heart} className="w-44 scaler" />
+              <div className="border-1 rounded-md p-5 text-lg ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+                obcaecati impedit est assumenda repellat nihil ea fugit magni?
+                Commodi harum illum magni provident, quibusdam laboriosam
+                voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nisi, doloribus.
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center justify-center gap-12">
-            <img src={wave} className="w-44 scaler" />
-            <div className="border-1 rounded-md p-5 text-xl ">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-              obcaecati impedit est assumenda repellat nihil ea fugit magni?
-              Commodi harum illum magni provident, quibusdam laboriosam
-              voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Nisi, doloribus.
+            <div className="flex flex-col items-center justify-center gap-12">
+              <img src={wave} className="w-44 scaler" />
+              <div className="border-1 rounded-md p-5 text-lg ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+                obcaecati impedit est assumenda repellat nihil ea fugit magni?
+                Commodi harum illum magni provident, quibusdam laboriosam
+                voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nisi, doloribus.
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center justify-center gap-12">
-            <img src={robot} className="w-44 scaler" />
-            <div className="border-1 rounded-md p-5 text-xl ">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-              obcaecati impedit est assumenda repellat nihil ea fugit magni?
-              Commodi harum illum magni provident, quibusdam laboriosam
-              voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Nisi, doloribus.
+            <div className="flex flex-col items-center justify-center gap-12">
+              <img src={robot} className="w-44 scaler" />
+              <div className="border-1 rounded-md p-5 text-lg ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+                obcaecati impedit est assumenda repellat nihil ea fugit magni?
+                Commodi harum illum magni provident, quibusdam laboriosam
+                voluptates itaque in sapiente fuga. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nisi, doloribus.
+              </div>
             </div>
           </div>
         </div>
-
         {/*  */}
-        <div className="bg-gray-300  p-20">
+        <div className="bg-gray-300 p-20">
           <img src={medNovaPic} className="scaler" />
         </div>
         {/*  */}
         <h1 className="mt-20 mb-10 text-center text-3xl font-bold">
           Meet our team
         </h1>
-        <div className="flex items-center justify-between">
-          <button className="text-6xl font-bold">‹</button>
-          <div className="flex items-center gap-16">
+        <div className="flex items-center md:flex-row flex-col justify-center overflow-x-scroll p-10">
+          {/* <button className="text-6xl font-bold">‹</button> */}
+          <div className="flex md:flex-row flex-col items-center gap-16">
             <ProfileCard
               name={"Shadow Mike"}
               position={"General Manager"}
@@ -167,13 +168,13 @@ const Home = () => {
               image={dummy}
             />
           </div>
-          <button className="text-6xl font-bold">›</button>
+          {/* <button className="text-6xl font-bold">›</button> */}
         </div>
         {/*  */}
-        <h1 className="mt-20 mb-16 text-center text-3xl font-bold">
+        <h1 className="mt-28 mb-16 text-center text-3xl font-bold">
           Our Socials
         </h1>
-        <div className="flex justify-center items-center gap-20">
+        <div className="flex md:flex-row flex-col justify-center items-center gap-20 p-10">
           <img src={phone} className="w-28 bg-white rounded-full" />
           <img src={X} className="w-24" />
           <img src={insta} className="w-28" />
